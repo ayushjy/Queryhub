42d4d12514a3cdf28498bf7a89a22fb28abb3a53
Ayush jyoti <ayushjyoti0321@gmail.com>
2025-06-13 02:18:19 +0530

    updated cookies settings

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -46,8 +46,8 @@ export const login = async (req, res) => {
             const token = generateToken(user);
             res.cookie('token', token, {
                 httpOnly: true,
-                secure:false, // Set to true in production
-            // secure: process.env.NODE_ENV === 'production', // Uncomment this line in production      
+                // secure:false, // Set to true in production
+            secure: process.env.NODE_ENV === 'production', // Uncomment this line in production      
                 sameSite: 'lax',
                 maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
             });
